f762842b5d8862d35cfa4ec99b6b27db98991c0d
Wistaro <wistaro@hotmail.fr>
2015-09-15 19:36:12 +0200

    Ajout des tokens

---
+++ tokens/explore.php
@@ -0,0 +1,14 @@
+<?php
+$row = 1;
+if (($handle = fopen("tokens.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num champs à la ligne $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
+?>
