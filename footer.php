7ccf7a987e8746992dc4c7fd5c82305feec6db0e
Mathis Lavigne <mat.live@live.fr>
2015-09-19 22:41:27 +0200

    Commit pour l'éditeur de texte

--- footer.php
+++ footer.php
@@ -7,17 +7,31 @@
 			mode: "tibasic",
 			styleActiveLine: true,
 			lineNumbers: true,
-			//viewportMargin: Infinity,
+			viewportMargin: Infinity,
 			theme: "3024-night"
 		});
+
 		document.getElementById("BT_triangle").addEventListener("click", function() {
 			editor.replaceRange("►", editor.getCursor());
 		});
 		document.getElementById("BT_arrow").addEventListener("click", function() {
 			editor.replaceRange("→", editor.getCursor());
 		});
+		document.getElementById("BT_theta").addEventListener("click", function() {
+			editor.replaceRange("θ", editor.getCursor());
+		});
+		document.getElementById("BT_sigma").addEventListener("click", function() {
+			editor.replaceRange("Σ", editor.getCursor());
+		});
+		document.getElementById("BT_delta").addEventListener("click", function() {
+			editor.replaceRange("Δ", editor.getCursor());
+		});
+
 		editor.on("change", function() {
-			alert('test');
+			if(editor.lineCount() > 20)
+				document.querySelector(".cm-s-3024-night.CodeMirror").style.height = "auto";
+			else
+				document.querySelector(".cm-s-3024-night.CodeMirror").style.height = "330px";
 		});
 	</script>
 </body>
--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	
 				<?php } elseif($mode == "input") { ?>
 	
-					<button id="BT_arrow" onclick="return false;">→</button><button id="BT_triangle" onclick="return false;">►</button>
+					<button id="BT_arrow" class="BT" onclick="return false;">→</button><button id="BT_triangle" class="BT" onclick="return false;">►</button><button id="BT_theta" class="BT" onclick="return false;">θ</button><button id="BT_sigma" class="BT" onclick="return false;">Σ</button><button id="BT_delta" class="BT" onclick="return false;">Δ</button>
 					<textarea name="code_input" placeholder="Saisissez votre code ici" class="TTREA_code" id="TTREA_code">If A=5</textarea> 
 					<input type="submit" action="envoyer" class="BT_send" />
 			
