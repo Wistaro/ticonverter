c0d83019b4b115668ed5216cb82266e99120a170
Mathis L. <abc.live@live.fr>
2015-09-28 00:00:31 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,39 +1,40 @@
-<script src="ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
-<!--<script src="codemirror-5.6/lib/codemirror.js"></script>-->
+
+<script src="codemirror-5.6/lib/codemirror.js"></script>
+<script src="codemirror-5.6/mode/tibasic/tibasic.js"></script>
 	<script type="text/javascript">
 		var editor = ace.edit("TTREA_code");
    		editor.setTheme("ace/theme/monokai");
    		editor.getSession().setMode("ace/mode/basic");
-		//var editor = CodeMirror.fromTextArea(document.getElementById("TTREA_code"), {
-		//	mode: "tibasic",
-		//	styleActiveLine: true,
-		//	lineNumbers: true,
-		//	viewportMargin: Infinity,
-		//	theme: "3024-night"
-		//});
+		var editor = CodeMirror.fromTextArea(document.getElementById("TTREA_code"), {
+			mode: "tibasic",
+			styleActiveLine: true,
+			lineNumbers: true,
+			viewportMargin: Infinity,
+			theme: "3024-night"
+		});
 //
-		//document.getElementById("BT_triangle").addEventListener("click", function() {
-		//	editor.replaceRange("►", editor.getCursor());
-		//});
-		//document.getElementById("BT_arrow").addEventListener("click", function() {
-		//	editor.replaceRange("→", editor.getCursor());
-		//});
-		//document.getElementById("BT_theta").addEventListener("click", function() {
-		//	editor.replaceRange("θ", editor.getCursor());
-		//});
-		//document.getElementById("BT_sigma").addEventListener("click", function() {
-		//	editor.replaceRange("Σ", editor.getCursor());
-		//});
-		//document.getElementById("BT_delta").addEventListener("click", function() {
-		//	editor.replaceRange("Δ", editor.getCursor());
-		//});
+		document.getElementById("BT_triangle").addEventListener("click", function() {
+			editor.replaceRange("►", editor.getCursor());
+		});
+		document.getElementById("BT_arrow").addEventListener("click", function() {
+			editor.replaceRange("→", editor.getCursor());
+		});
+		document.getElementById("BT_theta").addEventListener("click", function() {
+			editor.replaceRange("θ", editor.getCursor());
+		});
+		document.getElementById("BT_sigma").addEventListener("click", function() {
+			editor.replaceRange("Σ", editor.getCursor());
+		});
+		document.getElementById("BT_delta").addEventListener("click", function() {
+			editor.replaceRange("Δ", editor.getCursor());
+		});
 //
-		//editor.on("change", function() {
-		//	if(editor.lineCount() > 20)
-		//		document.querySelector(".cm-s-3024-night.CodeMirror").style.height = "auto";
-		//	else
-		//		document.querySelector(".cm-s-3024-night.CodeMirror").style.height = "330px";
-		//});
+		editor.on("change", function() {
+			if(editor.lineCount() > 20)
+				document.querySelector(".cm-s-3024-night.CodeMirror").style.height = "auto";
+			else
+				document.querySelector(".cm-s-3024-night.CodeMirror").style.height = "330px";
+		});
 	</script>
 	
 </body>
