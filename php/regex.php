ec7434416a3b688cb71739b3e6d9a017926c3640
Wistaro <wistaro@hotmail.fr>
2015-09-28 21:16:04 +0200

    Ajouts de quelques regex

--- php/regex.php
+++ php/regex.php
@@ -1,14 +1,58 @@
 <?php
 
-	$linecolor = '#^(Line|Ligne)\(+[a-zA-Z0-9_.-]+,[a-zA-Z0-9_.-]+,[a-zA-Z0-9_.-]+,[a-zA-Z0-9_.-]+,[a-zA-Z]#i'; //is line color 
-		
-	$ptoncolor = '#^(Pt-On|Pt-Aff)\(+[a-zA-Z0-9_.-]+,[a-zA-Z0-9_.-]+[a-zA-Z0-9_.-]?+,#i';
+	$linecolor = '#^(Line|Ligne)\(+[a-zA-Z0-9_.-]+,[a-zA-Z0-9_.-]+,[a-zA-Z0-9_.-]+,[a-zA-Z0-9_.-]+,[a-zA-Z]#i'; //is line colored 
+
+	$ptoncolor = '#^Pt-(On|Aff)\(([0-9A-Z+-]+\,){2}([0-9A-Z+-]+\,)?([a-zA-Z\s]+)\)#i'; //is pt colored
+
+	$horizcolor = '#^Horizontale?\s([0-9A-Z]+\,)?[A-Z\s]{3,8}#i'; //is horizontal colored
+
+	$verticcolor = '#^Verticale?\s([0-9A-Z]+\,)?[A-Z\s]{3,8}#i'; //is vertical colored
+
+	$textcolor = '#^Texte?\(([0-9A-Z-+]+\,){2}"[\w \s-+*\/\\\.]+"\,[A-Z\s]{3,8}#i'; //is text colored
+
+	$pxlcolor = '#^Pxl-(Aff|On)\(([0-9A-Z+-]+\,){2}[A-Z\s]{3,8}#i'; //is pxl colored
+
+	$ptchangecolor'#^Pt-changer?\((([0-9A-Z+-])+\,){2}([0-9A-Z+-]+\,)?[A-Z\s]{3,8}#i'; //is ptchange colored
 
 
 
-		if(preg_match($ptoncolor, "Pt-On(127,72,99,)")){
 
-			echo 'YES!';
 
-		}
+
+	$arraycolor = array('BLUE', 'RED', 'BLACK', 'MAGENTA', 'GREEN', 'ORANGE', 'BROWN', 'NAVY', 'LTBLUE', 'YELLOW', 'WHITE', 'LTGREY', 'MEDGRAY', 'GRAY', 'DARKGREY', 'BLEU', 'ROUGE', 'NOIR', 'VERT', 'BLEU RMN', 'BLEU CLR', 'JAUNE', 'BLANC', 'GRIS CLR', 'GRIS MOY', 'GRIS', 'GRIS FON');
+
+	
+/*
+	preg_match($ptoncolor, "Pt-Aff(10,10,White)", $rendu);
+
+	
+		if(!empty($rendu[0])){
+
+			preg_match("#^[\,]([a-zA-Z0-1]+)#i", $rendu[count($rendu)-1],$rendu1);
+			
+	}
+	
+
+	echo print_r($rendu1);
+	//print_r($rendu1);
+	
+
+				if(isset($rendu[count($rendu)-1]) and isset($rendu1[1])){
+
+							echo 'Couleur = '.substr($rendu[count($rendu)-1], 1);
+
+
+
+				}else{
+
+
+							echo 'Non spécifiée';
+				}
+
+
+			
+
+*/
+
+	
 ?>
