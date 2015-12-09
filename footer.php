dbe646995822c0696367abb0e838c67f6c99677d
Mathis L <abc.live@live.fr>
2015-12-09 23:57:10 +0100

    Syntaxic coloration increased

--- footer.php
+++ footer.php
@@ -12,30 +12,52 @@
 			theme: "base16-dark"
 		});
 
+		function changeLang(lang) {
+			var content = editor.getValue();
+			document.getElementsByClassName("CodeMirror").remove();
+			editor = CodeMirror.fromTextArea(document.getElementById("TTREA_code"), {
+				mode: lang,
+				styleActiveLine: true,
+				lineNumbers: true,
+				viewportMargin: Infinity,
+				theme: "base16-dark"
+			});
+			editor.setValue(content);
+		}
+
 		document.getElementById("BT_triangle").addEventListener("click", function() {
 			editor.replaceRange("►", editor.getCursor());
+			editor.focus();
 		});
 		document.getElementById("BT_arrow").addEventListener("click", function() {
 			editor.replaceRange("→", editor.getCursor());
+			editor.focus();
 		});
 		document.getElementById("BT_theta").addEventListener("click", function() {
 			editor.replaceRange("θ", editor.getCursor());
+			editor.focus();
 		});
 		document.getElementById("BT_sigma").addEventListener("click", function() {
 			editor.replaceRange("Σ", editor.getCursor());
+			editor.focus();
 		});
 		document.getElementById("BT_delta").addEventListener("click", function() {
 			editor.replaceRange("∆", editor.getCursor());
+			editor.focus();
 		});
 		document.getElementById("BT_list").addEventListener("click", function() {
 			editor.replaceRange("⌊", editor.getCursor());
+			editor.focus();
 		});
 		document.getElementById("BT_neg").addEventListener("click", function() {
 			editor.replaceRange("⁻", editor.getCursor());
+			editor.focus();
 		});
 		document.getElementById("BT_subT").addEventListener("click", function() {
 			editor.replaceRange("ᴛ", editor.getCursor());
+			editor.focus();
 		});
+
 
 		editor.on("change", function() {
 			if(editor.lineCount() > 20)
@@ -45,6 +67,18 @@
 		});
 		if(editor.lineCount() > 20)
 			document.querySelector(".cm-s-base16-dark.CodeMirror").style.height = "auto";
+
+
+		Element.prototype.remove = function() {
+			this.parentElement.removeChild(this);
+		}
+		NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+			for(var i = this.length - 1; i >= 0; i--) {
+				if(this[i] && this[i].parentElement) {
+					this[i].parentElement.removeChild(this[i]);
+				}
+			}
+		}
 	</script>
 	
 </body>
