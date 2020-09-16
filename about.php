d8c57407425268c0cf4f7dc0c0e0e4b350944c23
Wistaro <wistaro@hotmail.fr>
2020-09-16 16:25:01 +0200

    Update about.php

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
 	<section class="SEC_main">
 		<h1>A propos de </h1>
 		
-			<p>Ce convertisseur a été développé par William "Wistaro" Romiguieres, Mathis "Epharius" Lavigne, Adrien "Adriweb" Bertrand.</p><br /><br />
+			<p>Ce convertisseur a été développé par Wistaro , Epharius, et Adriweb.</p><br /><br />
 			<br ><p>L'application est encore en version béta! Si vous remarquez un bug, si vous avez une suggestion, c'est <a href="https://tiplanet.org/forum/viewtopic.php?f=10&t=17212">par ici!</a></p>
 				
 
